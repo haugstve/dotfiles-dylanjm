0d66384e9e65bb1bb650a56037a97c80c4d63699
Dylan McDowell <dylan.mcdowell226@gmail.com>
2017-05-13 14:27:51 -0600

    Homely.py

--- HOMELY.py
+++ HOMELY.py
@@ -1,5 +1,6 @@
 # ~/dotfiles/HOMELY.py
 # NOTE that we use homely's mkdir() not os.mkdir()
+import os.path
 from homely.files import mkdir
 from homely.files import symlink
 
@@ -14,9 +15,17 @@ symlink('Khd/khdrc', '~/.khdrc')
 symlink('Zsh/zshrc', '~/.zshrc')
 
 # Git
+gfile = Path('~/.gitconfig')
+if gfile.isfile():
+	os.remove(gfile)
 symlink('Git/gitconfig', '~/.gitconfig')
 symlink('Git/global_gitignore', '~/.global_gitignore')
 
 # Sublime Text
 symlink('/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl',
 	'/usr/local/bin/subl')
+sdir = Path('~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User')
+if sdir.is_dir():
+	os.rmdir(sdir)
+symlink('Sublime/Packages/User',
+	'~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User')
