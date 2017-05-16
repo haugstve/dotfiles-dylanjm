11d75ed4696bcea3e61e3fba9efb23adbc4e9933
Dylan McDowell <dylan.mcdowell226@gmail.com>
2017-05-15 23:13:55 -0600

    Subl

--- HOMELY.py
+++ HOMELY.py
@@ -19,11 +19,28 @@ symlink('Git/global_gitignore', '~/.global_gitignore')
 
 # Sublime Text
 # subl CLI link
-symlink('/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl','/usr/local/bin/subl')
+symlink('/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl',
+        '/usr/local/bin/subl')
 # Sublime Preferences link
 symlink('~/dotfiles/Sublime/Packages/User/Preferences.sublime-settings',
-	'~/Library/Application Support/Sublime Text 3/Packages/User/Preferences.sublime-settings')
+        '~/Library/Application Support/Sublime Text 3/Packages/User/Preferences.sublime-settings')
 # Spacegrey Theme link
 symlink('~/dotfiles/Sublime/Packages/Theme - Spacegray',
-	'~/Library/Application Support/Sublime Text 3/Packages/Theme - Spacegray')
+		'~/Library/Application Support/Sublime Text 3/Packages/Theme - Spacegray')
+# Package Control link
+symlink('~/dotfiles/Sublime/Packages/User/Package Control.sublime-settings',
+		'~/Library/Application Support/Sublime Text 3/Packages/User/Package Control.sublime-settings')
+# Python Specific link
+symlink('~/dotfiles/Sublime/Packages/User/Python.sublime-settings',
+		'~/Library/Application Support/Sublime Text 3/Packages/User/Python.sublime-settings')
+# R-Box link
+symlink('~/dotfiles/Sublime/Packages/User/R-Box.sublime-settings',
+		'~/Library/Application Support/Sublime Text 3/Packages/User/R-Box.sublime-settings')
+# Send Code link
+symlink('~/dotfiles/Sublime/Packages/User/SendCode.sublime-settings',
+		'~/Library/Application Support/Sublime Text 3/Packages/User/SendCode.sublime-settings')
+# LaTeXTools link
+symlink('~/dotfiles/Sublime/Packages/User/LaTeXTools.sublime-settings',
+		'~/Library/Application Support/Sublime Text 3/Packages/User/LaTeXTools.sublime-settings')
+
 
--- Sublime/sublimeIcon.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-# Sublime
-#
-# This installs a new icon for Sublime Text
-
-cp $HOME/dotfiles/Sublime/Sublime\ Text.icns /Applications/Sublime\ Text.app/Contents/Resources/
-touch /Applications/Sublime\ Text.app
-sudo killall Dock & sudo killall Finder
