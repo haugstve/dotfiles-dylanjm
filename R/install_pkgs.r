e35ff2a4802edef4efe5c31fd9a045b1ed89307c
Dylan McDowell <dylan.mcdowell226@gmail.com>
2017-05-30 13:20:14 -0600

    Stuff

--- R/install_pkgs.r
+++ R/install_pkgs.r
@@ -2,4 +2,4 @@
 
 print("Reading in r_packages.txt!")
 packages <- scan("~/dotfiles/R/r_packages.txt", what = "", sep = "\n")
-install.packages(packages, repo = 'https://cloud.r-project.org/')
+install.packages(packages, repo = 'https://cloud.r-project.org/',dependencies=TRUE)
